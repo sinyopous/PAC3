81b1694e34d69ef151590583361631662d959648
sinyopous <121230011+sinyopous@users.noreply.github.com>
2023-01-31 19:48:45 +0100

    1.6
    -remake of script doc
     -clear code
     -modular
     -functionality to choose the  number of pokemon to print

---
+++ poke.js
@@ -0,0 +1,171 @@
+const pokeApi = "https://pokeapi.co/api/v2/pokemon?limit=10000";
+const totalNumOfPokemon = 1279;
+let numOfPokemonToFetch =
+  document.getElementById("randomSearchInput").innerHTML;
+let raiseButton = document.getElementById("raiseNumber");
+let lowerButton = document.getElementById("lowerNumber");
+let luckyButton = document.getElementById("luckyButton");
+//----------------FUNCTIONS--------------------
+
+const getRandomNum = (maxNum) => {
+  return Math.floor(Math.random() * maxNum);
+};
+
+const createRandomNumsArray = (maxNum, arrayLength) => {
+  let randomNumsArray = [];
+  while (arrayLength > randomNumsArray.length) {
+    let num = getRandomNum(maxNum);
+    if (randomNumsArray.every((x) => x !== num)) {
+      randomNumsArray.push(num);
+    }
+  }
+  return randomNumsArray;
+};
+
+const consultFetch = (url) => {
+  return fetch(url).then((x) => x.json());
+};
+
+const getIdsAccesUrls = () => {
+  return pokeFetch.then((x) => {
+    return x.results.map((y) => y.url);
+  });
+};
+
+const getXRandomAccesUrls = () => {
+  return idAccesUrls.then((urlList) => {
+    let pokeUrls = [];
+    for (let url of urlList) {
+      for (let num of randomNumArray) {
+        if (num === urlList.indexOf(url)) {
+          pokeUrls.push(url);
+        }
+      }
+    }
+    return pokeUrls;
+  });
+};
+
+const printXRandomCards = () => {
+  randomIdAccesUrls.then((urlList) => {
+    urlList.forEach((url) => {
+      fetch(url)
+        .then((x) => x.json())
+        .then((y) => {
+          //--------------------LOGS-----------------
+          //   console.dir(y);
+          //   console.log(y.name);
+          //   console.log(y.id);
+          //   console.log(y.sprites.front_default);
+          //   console.log(y.sprites.back_default);
+          //   console.log(`attack: ${y.stats[1].base_stat}`);
+          //   console.log(`defense: ${y.stats[2].base_stat}`);
+
+          //   y.types.forEach((z) => {
+          //     console.log(z.type.name);
+          //   });
+          //-------------PRINT TEMPLATE-------------
+
+          const out = document.getElementById("pokeListBox");
+          const template = document.getElementById("templateSimpleCard");
+          const clonedTemplate = template.content.cloneNode(true);
+
+          let card = clonedTemplate.querySelector(".pokeSimpleCard");
+          let image = clonedTemplate.querySelector(".pokePortrait");
+          let pokeName = clonedTemplate.querySelector(".pokeName2");
+
+          card.setAttribute("id", y.id);
+          if (y.sprites.front_default) {
+            image.setAttribute("src", y.sprites.front_default);
+          } else {
+            image.setAttribute(
+              "src",
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png"
+            );
+          }
+          pokeName.innerHTML = y.name;
+          // card.addEventListener('click', funcionasion);
+
+          out.appendChild(clonedTemplate);
+        });
+    });
+  });
+};
+
+const swapDisplay = (target) => {
+  document.getElementById("pokeInputFlexbox").style.display = "none";
+  document.getElementById("pokeSearchBar").style.display = "none";
+  let simpleCards = document.querySelectorAll(".pokeSimpleCard");
+  simpleCards.forEach((x) => (x.style.display = "none"));
+
+  document.querySelector(".pokeStatsCard").style.display = "flex";
+  document.getElementById(`${target.id}`).style.display = "none";
+};
+
+//------------------GET LUCKY BUTTON----------------------
+const raiseNumber = () => {
+  document.getElementById("randomSearchInput").innerHTML++;
+  numOfPokemonToFetch++;
+};
+
+const lowerNumber = () => {
+  if (document.getElementById("randomSearchInput").innerHTML > 1) {
+    document.getElementById("randomSearchInput").innerHTML--;
+    numOfPokemonToFetch--;
+  }
+};
+
+const LetsGetLucky = () => {
+  hideScreenElements();
+  randomNumArray = createRandomNumsArray(
+    totalNumOfPokemon,
+    numOfPokemonToFetch
+  );
+  randomIdAccesUrls = getXRandomAccesUrls();
+  printXRandomCards();
+};
+
+
+//----------------------WORK IN ORDERD POKELIST, CREATE A FUNC THAT GENERATES AN ARRAY OF NUMBERS FROM 0 TO MAX OF POKEMON-------------
+
+const orderedPokemon = (array) => {
+  hideScreenElements();
+  randomNumArray = array;
+  randomIdAccesUrls = getXRandomAccesUrls();
+  printXRandomCards();
+};
+
+const hideScreenElements = () => {
+  document.getElementById("pokeInputFlexbox").style.display = "none";
+  document.getElementById("pokeSearchBar").style.display = "none";
+  let simpleCards = document.querySelectorAll(".pokeSimpleCard");
+  simpleCards.forEach((x) => (x.style.display = "none"));
+};
+
+//-------------------VARIABLES-------------------------------
+
+let pokeFetch = consultFetch(pokeApi);
+let randomNumArray = createRandomNumsArray(
+  totalNumOfPokemon,
+  numOfPokemonToFetch
+);
+
+let idAccesUrls = getIdsAccesUrls();
+let randomIdAccesUrls = getXRandomAccesUrls();
+
+//-----------------EVENT LISTENERS-------------------------
+raiseButton.addEventListener("click", raiseNumber);
+raiseButton.addEventListener("drag", raiseNumber);
+lowerButton.addEventListener("click", lowerNumber);
+lowerButton.addEventListener("drag", lowerNumber);
+luckyButton.addEventListener("click", LetsGetLucky);
+
+//--------------COM USAR LA INFO QUE RETORNA UNA PROMESA (.then)-----------------------
+
+idAccesUrls.then((x) => console.dir(x));
+
+//--------------------------------------------------------------------------
+//--------------------------LOGCHECKS---------------------------------------
+console.dir(randomNumArray);
+randomIdAccesUrls.then((x) => console.dir(x));
+printXRandomCards();
